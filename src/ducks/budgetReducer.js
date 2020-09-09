9ed0fffd02e3b4f38d2b8eba37715a7c1cf14a60
Cj <carterjpilling@gmail.com>
2020-09-09 16:04:35 -0600

    finished step 6

--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -1,9 +1,29 @@
+import axios from 'axios'
+
 const initialState = {
   purchases: [],
   budgetLimit: null,
   loading: false
 }
 
-export default function reduer(state = initialState, action) {
-  return state
+const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA'
+
+export const requestBudgetData = () => {
+  let data = axios.get('/api/budget-data').then(res => res.data)
+  return {
+    type: REQUEST_BUDGET_DATA,
+    payload: data
+  }
+}
+
+
+export default function budgetReducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_BUDGET_DATA + '_PENDING':
+      return { ...state, loading: true }
+    case REQUEST_BUDGET_DATA + '_FULFILLED':
+      return { ...state, ...action.payload, loading: false }
+    default:
+      return state
+  }
 }
